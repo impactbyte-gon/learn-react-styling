766948a2d1456f819a51ac9f48dc0e8cb1b43abc
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-13 14:42:32 +0700

    Use styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Content = styled.div`
+  text-align: center;
+  color: black;
+`
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Content>
+        <h1>Hello World!</h1>
+        <p>This is a sentence.</p>
+      </Content>
     )
   }
 }
